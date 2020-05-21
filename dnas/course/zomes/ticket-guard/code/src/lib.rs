26ea0af710ae5a912f4038a1858de581081777ea
Hedayat Abedijoo <h.abedijoo@gmail.com>
2020-05-21 14:34:31 +0200

    implementing course-guard validation

--- dnas/course/zomes/ticket-guard/code/src/lib.rs
+++ dnas/course/zomes/ticket-guard/code/src/lib.rs
@@ -1,18 +1,25 @@
 #![feature(proc_macro_hygiene)]
-#![allow(dead_code)]
+extern crate hdk;
+extern crate hdk_proc_macros;
+extern crate holochain_entry_utils;
+extern crate holochain_json_derive;
+extern crate serde;
+extern crate serde_derive;
+extern crate serde_json;
 
+// use hdk::holochain_persistence_api::cas::content::Address;
+// use hdk::{entry_definition::ValidatingEntryType, error::ZomeApiResult};
 use hdk_proc_macros::zome;
 
-// see https://developer.holochain.org/api/0.0.47-alpha1/hdk/ for info on using the hdk library
+pub mod settings;
+pub mod ticket;
 
-// This is a sample zome that defines an entry type "MyEntry" that can be committed to the
-// agent's chain via the exposed function create_my_entry
-mod settings;
-mod ticket;
 #[zome]
 mod my_zome {
 
+    use crate::{settings, ticket};
     use hdk::EntryValidationData;
+    use ticket::Ticket;
 
     #[init]
     fn init() {
@@ -21,37 +28,57 @@ mod my_zome {
 
     #[validate_agent]
     pub fn validate_agent(validation_data: EntryValidationData<AgentId>) {
-        let nick_name = "nick_name_with_data".to_string(); //hdk::prelude::AgentId::decoded_key()?;
-        let _ticket = ticket::decode(nick_name)?;
-        let _prop_teacher_address = crate::settings::get_teacher_address()?;
-        let _prop_dna_id = crate::settings::get_dna_id()?;
-        let _agent_address = validation_data.package.chain_header.provenances()[0].source();
-
-        /// if   _prop_teacher_address == _agent_address   return OK(())
-        /// else check ticket
-        // if ticket.ticket_info.buyer== *  =>
-        ///   then if thicket.ticket_info.Get_Signature() == ticket.signature()  /// it freee   reutrn OK(())  else/// else
-        /// else then    if ticket.ticket_info.buyer_address == _agent_address   and  validate_Siganture /// ture
         match validation_data {
             EntryValidationData::Create {
                 validation_data, ..
             } => {
                 let agent_address = validation_data.package.chain_header.provenances()[0].source();
-                hdk::debug(format!(
-                    "hedayat: agent address in validation_data {:?}",
-                    agent_address.clone()
-                ))?;
 
-                Ok(())
-                //  Ok(())
-                // match members::is_valid_member(&agent_address)? {
-                //     true => Ok(()),
-                //     false => Err(format!("Validation Error: Agent {} is not valid since it does not comply with social triangulation conditions", agent_address))
-                // }
+                let nick_name = "dummy_data".to_string();
+
+                let ticket: Ticket = ticket::decode(nick_name)?;
+                let prop_course_id = settings::get_dna_id()?;
+                let prop_teacher_addr = settings::get_teacher_address()?;
+
+                if agent_address == prop_teacher_addr {
+                    return Ok(());
+                }
+                if ticket.ticket_info.source_item_id != prop_course_id {
+                    return Err(String::from(
+                        "Validation Error:  ticket is not issed for this DNA",
+                    ));
+                }
+
+                if ticket.ticket_info.buyer_address == "*".to_string() // free course
+                    && ticket.signature != ticket.ticket_info.generate_signature()
+                {
+                    return Err(String::from("Validation Error:  ticket is not valid"));
+                }
+
+                if ticket.ticket_info.buyer_address != agent_address.to_string() {
+                    return Err(String::from(
+                        "Validation Error:  ticket is not issued for you",
+                    ));
+                }
+                if ticket.ticket_info.buyer_address == agent_address.to_string()
+                    && ticket.signature != ticket.ticket_info.generate_signature()
+                {
+                    return Err(String::from("Validation Error:  ticket is not valid"));
+                }
             }
-            _ => Err(String::from("Validation Error:  validating the agent")),
-        } // TODO:
-          // if ()
+
+            EntryValidationData::Modify {
+                new_entry,
+                old_entry,
+                old_entry_header,
+                validation_data,
+            } => {}
+            EntryValidationData::Delete {
+                old_entry,
+                old_entry_header,
+                validation_data,
+            } => {}
+        }
 
         Ok(())
     }
--- dnas/course/zomes/ticket-guard/code/src/ticket.rs
+++ dnas/course/zomes/ticket-guard/code/src/ticket.rs
@@ -1,27 +1,65 @@
+use hdk::prelude::*;
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct Ticket {
-    ticket_info: TicketInfo,
-    signature: String,
+    pub ticket_info: TicketInfo,
+    pub signature: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct TicketInfo {
-    source_item_id: String,
-    issuer_address: String,
-    timestamp: usize,
-    buyer_address: String,
+    pub source_item_id: String,
+    pub issuer_address: String,
+    pub timestamp: usize,
+    pub buyer_address: String,
 }
 
 impl TicketInfo {
-    pub fn Get_Signature(&self) -> String {
+    pub fn generate_signature(&self) -> String {
         return "get signature using issuer_address".to_string();
+    }
+
+    pub fn new(
+        source_item_id: String,
+        issuer_address: String,
+        timestamp: usize,
+        buyer_address: String,
+    ) -> Self {
+        TicketInfo {
+            source_item_id,
+            issuer_address,
+            timestamp,
+            buyer_address,
+        }
     }
 }
 
-pub fn decode(_token: String) -> Result<Ticket, String> {
-    Ok(Ticket {
-        source_item_id: "dna_Id".to_string(),
-        issuer_address: "asdfasdfsfd".to_string(),
-        timestamp: 12,
-        ticket_type: TicketType::Free,
-        signature: "something".to_string(),
-    })
+impl Ticket {
+    pub fn new(ticket_info: TicketInfo) -> Self {
+        Ticket {
+            ticket_info: ticket_info.clone(),
+            signature: ticket_info.generate_signature(),
+        }
+    }
+
+    pub fn issue_free_ticket(
+        source_item_id: String,
+        issuer_address: String,
+        timestamp: usize,
+    ) -> Self {
+        let ticket_info =
+            TicketInfo::new(source_item_id, issuer_address, timestamp, "*".to_string());
+
+        return Ticket::new(ticket_info);
+    }
+}
+
+pub fn decode(token: String) -> Result<Ticket, String> {
+    let ticket = Ticket::new(TicketInfo::new(
+        "source_item_id".to_string(),
+        "issuer_address".to_string(),
+        123,
+        token,
+    ));
+
+    return Ok(ticket);
 }
