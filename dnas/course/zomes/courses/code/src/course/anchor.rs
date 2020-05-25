2ff5a3c9521fde994dd84a0c0e32b96f8d1d3d55
e-nastasia <32966301+e-nastasia@users.noreply.github.com>
2020-05-25 14:15:02 +0300

    separately defined CourseAnchor; validation for Course, CourseAnchor

---
+++ dnas/course/zomes/courses/code/src/course/anchor.rs
@@ -0,0 +1,96 @@
+// CourseAnchor provides a constant address for a course to be referenced by.
+// Once created, it never changes. It is linked to all Course entries to provide access to the course data.
+pub struct CourseAnchor {
+    timestamp: i32,
+    teacher_address: Address,
+}
+
+pub const COURSE_ANCHOR_ENTRY_NAME: &str = "course_anchor";
+pub const COURSE_ANCHOR_ENTRY_DESCRIPTION: &str = "CourseAnchor entry provides constant address for a course to be referenced by";
+pub const LINK_COURSE_ANCHOR_TO_COURSE: &str = "course_anchor->course";
+pub const LINK_TEACHER_TO_COURSE_ANCHOR: &str = "teacher->courses";
+pub const LINK_STUDENT_TO_COURSE_ANCHOR: &str = "student->courses";
+pub const LINK_COURSE_ANCHOR_TO_STUDENT: &str = "course->students";
+
+impl HolochainEntry for CourseAnchor {
+    fn entry_type() -> String {
+        String::from(COURSE_ANCHOR_ENTRY_NAME)
+    }
+}
+
+pub fn course_anchor_entry_def() -> ValidatingEntryType {
+    entry!(
+        name: COURSE_ANCHOR_ENTRY_NAME,
+        description: COURSE_ANCHOR_ENTRY_DESCRIPTION,
+        sharing: Sharing::Public,
+        validation_package: || {
+            hdk::ValidationPackageDefinition::Entry
+        },
+        validation: | validation_data: hdk::EntryValidationData<Course>| {
+            match validation_data{
+                EntryValidationData::Create { entry, validation_data } => {
+                    // NOTE (e-nastasia): these calls are identical to validation calls in course/entry.rs
+                    //      but there are methods with different signature matching CourseAnchor type
+                    validation::create(entry,validation_data)?
+                },
+                EntryValidationData::Modify { new_entry, old_entry, validation_data, .. } => {
+                    validation::modify(new_entry,old_entry,old_entry_header,validation_data)?
+                },
+                EntryValidationData::Delete {old_entry, validation_data, .. } => {
+                    validation::delete(old_entry, old_entry_header, validation_data)?
+                }
+            }
+        },
+        links: [
+            to!( // to query this course's latest data
+                COURSE_ANCHOR_ENTRY_NAME,
+                link_type: LINK_COURSE_ANCHOR_TO_COURSE,
+                validation_package: || {
+                    hdk::ValidationPackageDefinition::Entry
+                },
+                validation: | _validation_data: hdk::LinkValidationData | {
+                    // TODO:  should validate that we're only linking against Course that has
+                    // this entry's address in course_anchor field
+                    Ok(())
+                }
+            ),
+            from!( // to query all the courses of a user(all courses that a user is the teacher or owner of)
+                "%agent_id",
+                link_type: LINK_TEACHER_TO_COURSE_ANCHOR,
+                validation_package: || {
+                    hdk::ValidationPackageDefinition::Entry
+                },
+                validation: | _validation_data: hdk::LinkValidationData | {
+                    // TODO: validate that we're only linking to courses that have teacher_address equal to %agent_id
+                    Ok(())
+                }
+            ),
+            // e-nastasia: Q: would this link work & make sense with DNA-per-course architecture? Because with it
+            //       every course that certain agent is enrolled to is just another DNA that this agent is running. Of course,
+            //       we still need to differentiate between courses where agent is a teacher vs courses where agent is a student
+            from!( // to query all courses that one user enrolled
+                "%agent_id",
+                link_type: LINK_STUDENT_TO_COURSE_ANCHOR,
+                validation_package: || {
+                    hdk::ValidationPackageDefinition::Entry
+                },
+                validation: | _validation_data: hdk::LinkValidationData | {
+                    Ok(())
+                }
+            ),
+            // e-nastasia: Q: pretty much the same applies here. This link is in an opposite direction, but it's usefullness is still
+            //      questionable in DNA-per-course architecture
+            to!( // to query all enrolled user for a course
+                "%agent_id",
+                link_type: LINK_COURSE_ANCHOR_TO_STUDENT,
+                validation_package: || {
+                    hdk::ValidationPackageDefinition::Entry
+                },
+                validation: | _validation_data: hdk::LinkValidationData | {
+                    // TODO: validate that we're not linking to course teacher
+                    Ok(())
+                }
+            )
+        ]
+    )
+}
--- dnas/course/zomes/courses/code/src/course/validation.rs
+++ dnas/course/zomes/courses/code/src/course/validation.rs
@@ -1,57 +1,65 @@
 use super::entry::Course;
+use super::anchor::CourseAnchor;
 use hdk::holochain_core_types::chain_header::ChainHeader;
 use hdk::holochain_persistence_api::cas::content::Address;
 
 use hdk::ValidationData;
-use strings::*;
+
+// ========== Course validation ========
 
 pub fn create(entry: Course, validation_data: ValidationData) -> Result<(), String> {
-    // you need to compare Signature of Entry with Teacher_Address of DNA. in this case there is no need to put Teacher_Address in all entries.
-    // the same with Edit, Delete
-    if validation_data
-        .sources()
-        .contains(&crate::helper::get_teacher_address()?)
-    {
-        Err("You are not the teacher of this DNA, so you can not create a course".to_string())
-    } else {
-        Ok(())
-    }
+    validate_only_teacher_can_do(validation_data.sources(), "create")
 }
 
 pub fn modify(
     _new_entry: Course,
     _old_entry: Course,
     _old_entry_header: ChainHeader,
-    _validation_data: ValidationData,
+    validation_data: ValidationData,
 ) -> Result<(), String> {
-    Err("You can not edit payment info entry".to_string())
+    validate_only_teacher_can_do(validation_data.sources(), "modify")
 }
 
 pub fn delete(
     _old_entry: Course,
     _old_entry_header: ChainHeader,
-    _validation_data: ValidationData,
+    validation_data: ValidationData,
 ) -> Result<(), String> {
-    Err("You can not delete payment info entry".to_string())
+    validate_only_teacher_can_do(validation_data.sources(), "delete")
 }
 
-pub fn validate_only_teacher_can_do(
+// ========== CourseAnchor validation ========
+
+pub fn create(entry: CourseAnchor, validation_data: ValidationData) -> Result<(), String> {
+    validate_only_teacher_can_do(validation_data.sources(), "create")
+}
+
+pub fn modify(
+    _new_entry: CourseAnchor,
+    _old_entry: CourseAnchor,
+    _old_entry_header: ChainHeader,
+    validation_data: ValidationData,
+) -> Result<(), String> {
+    Err("You can not modify CourseAnchor".to_string())
+}
+
+pub fn delete(
+    _old_entry: CourseAnchor,
+    _old_entry_header: ChainHeader,
+    validation_data: ValidationData,
+) -> Result<(), String> {
+    validate_only_teacher_can_do(validation_data.sources(), "delete")
+}
+
+// ========== private validation helpers ========
+
+fn validate_only_teacher_can_do(
     validation_data_sources: Vec<Address>,
-    teacher_addr: &Address,
     action_name: &str,
 ) -> Result<(), String> {
-    if !validation_data_sources.contains(teacher_addr) {
-        return Err(format!(ERR_ONLY_TEACHER_CAN, action_name));
+    if !validation_data_sources.contains(&crate::helper::get_teacher_address()?) {
+        return Err(format!("Only the teacher can {} courses", action_name));
     }
     Ok(())
 }
 
-pub fn validate_no_teacher_change(
-    old_teacher_addr: &Address,
-    new_teacher_addr: &Address,
-) -> Result<(), String> {
-    if old_teacher_addr != new_teacher_addr {
-        return Err(String::from(ERR_NO_TEACHER_CHANGE));
-    }
-    Ok(())
-}
